6e450a0c0e386daf718c8012fb559c2a65a8e1ec
Jesse <jkolb@ucalgary.ca>
2015-01-22 03:13:22 -0700

    added my code for the programming assignment to cachematrix.r

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Jesse Kolb
+#1/22/2015
+#"R Programming" Coursera course programming assignment 2
+#Code was adapted from "cacheMean.R" and "makeVector.R" provided in the course.
 
-## Write a short comment describing this function
+##makeCacheMatrix: Returns list of functions to cache and retrieve inverted matrices
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y         #double arrows searches through parent environments
+        m <<- NULL      #double arrows searches through parent environments
+    }
+    get <- function() x         #defines get as returning original data
+    setinv <- function(solve) m <<- solve       #defines setinv as inverting a matrix
+    getinv <- function() m      #defines getinv as returning the inverted matrix
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)       #creates a list of functions to be used in cacheSolve.R
 }
 
 
-## Write a short comment describing this function
+##cacheSolve:Retrieves cached inverted matrix if there is one.
+##If there isn't, inverts the matrix and caches result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()         #tries to get cached inverse
+    if(!is.null(m)) {       #tests if the inverse is cached
+        message("getting cached data")
+        return(m)       #if the inverse is cached it returns it
+    }
+    data <- x$get()         #if the inverse is not cached then data becomes data...
+    m <- solve(data, ...)       #...and then the inverse is calculated...
+    x$setinv(m)         #...and the inverse which was just calculated is now cached
+    m       #returns m (the inverse matrix) as the result of the function
 }
